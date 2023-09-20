c095d62b280c9f05b1f2340e0ebf33aa59eafbd2
Universal Studio <10200399+TMUniversal@users.noreply.github.com>
2023-09-20 17:16:56 +0200

    feat(cmd): add command to generate man pages
    Signed-off-by: Universal Studio <10200399+TMUniversal@users.noreply.github.com>

---
+++ cmd/man.go
@@ -0,0 +1,64 @@
+/*
+ * This file is part of PaperCrypt.
+ *
+ * PaperCrypt lets you prepare encrypted messages for printing on paper.
+ * Copyright (C) 2023 TMUniversal <me@tmuniversal.eu>.
+ *
+ * PaperCrypt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package cmd
+
+import (
+	"os"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
+)
+
+// manCmd represents the man command
+var manCmd = &cobra.Command{
+	Use:   "man",
+	Short: "Generate man page",
+	Long: `Generate man pages for PaperCrypt commands.
+
+Generated pages will be placed in the directory specified by --out, defaulting to './man'.`,
+	Args: cobra.NoArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		// 1. outFileName is the directory here, if it is unset, default to man
+		if outFileName == "" {
+			outFileName = "man"
+		}
+
+		// 2. Generate files
+		err := GenerateManPage(outFileName)
+		if err != nil {
+			cmd.Print(errors.Wrap(err, "Error generating man page"))
+			os.Exit(1)
+		}
+	},
+}
+
+func GenerateManPage(dir string) error {
+	header := &doc.GenManHeader{
+		Source: "PaperCrypt " + VersionInfo.Version,
+	}
+
+	return doc.GenManTree(rootCmd, header, dir)
+}
+
+func init() {
+	rootCmd.AddCommand(manCmd)
+}
--- go.mod
+++ go.mod
@@ -15,10 +15,13 @@ require (
 	github.com/ProtonMail/go-crypto v0.0.0-20230828082145-3c4c8a2d2371 // indirect
 	github.com/ProtonMail/go-mime v0.0.0-20230322103455-7d82a3887f2f // indirect
 	github.com/cloudflare/circl v1.3.3 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/crypto v0.12.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
