ad50f082be86f4d43e351afa0fca38d8f855464e
Universal Studio <10200399+TMUniversal@users.noreply.github.com>
2023-09-24 00:22:33 +0200

    chore(script): update completion script
    use `go run .` over `go run main.go`

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -5,5 +5,5 @@ rm -rf completions
 mkdir completions
 for shell in bash zsh fish; do
   echo "Generating completions for $shell"
-  go run main.go completion "$shell" > "completions/papercrypt.$shell"
+  go run . completion "$shell" > "completions/papercrypt.$shell"
 done
