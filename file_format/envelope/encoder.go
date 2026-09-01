e950a3a608f80a23f28da35037febbc65b920f8f
Universal Studio <10200399+TMUniversal@users.noreply.github.com>
2026-09-01 14:44:40 +0200

    docs(envelope): note EncodingTypeRaw is reserved
    Removing it and promoting Base45 to value 0 would change the wire header,
    so document the raw value as intentionally reserved instead. Adds the
    standard AGPL header.

--- file_format/envelope/encoder.go
+++ file_format/envelope/encoder.go
@@ -1,3 +1,23 @@
+/*
+ * This file is part of PaperCrypt.
+ *
+ * PaperCrypt lets you prepare encrypted messages for printing on paper.
+ * Copyright (C) 2026 TMUniversal <me@tmuniversal.eu>.
+ *
+ * PaperCrypt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package envelope
 
 import (
@@ -9,6 +29,9 @@ import (
 type EncodingType uint8
 
 const (
+	// EncodingTypeRaw is reserved for a future raw encoder; no encoder
+	// currently registers it, and it must remain value 0 to keep the wire
+	// header stable.
 	EncodingTypeRaw EncodingType = iota
 	EncodingTypeBase45
 )
